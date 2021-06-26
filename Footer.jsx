c564524b22363a7013703417066b1f0ef1d066c8
M Srilekha <satyanarayanamuthyam@gmail.com>
2021-06-27 02:25:29 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer() {
+  return (
+    <footer>
+      <p>CopyRight by ShapeAi @ {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+export default Footer;
